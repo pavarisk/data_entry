b9f607d1f44aba2dbde243b5429b59d1a59fb939
pavarisk <p.kakanat@gmail.com>
2021-10-12 16:25:17 +1300

    Basic Dropdown Structure Started

---
+++ form/src/dropDown.js
@@ -0,0 +1,5 @@
+export const systemTheme = [{
+  Education: {
+    Education: ['Application', 'Appointment', 'Care Children (0-18)', 'Communication', 'Course / Programme', 'Early Childhood Education', 'Enrolment', 'Financial Assistance', 'Life Skills', 'Referral', 'Repairs', 'Review Hui', 'Safety', 'School', 'Support', 'Whanau Hui']
+  }
+}]
