650efd4e75988b8ee71c6bc2a5f45f08fb32bd30
pavarisk <p.kakanat@gmail.com>
2021-10-14 12:12:59 +1300

    dropdown js cont

--- form/src/dropDown.js
+++ form/src/dropDown.js
@@ -1,5 +1,65 @@
-export const systemTheme = [{
+export const systemTheme = {
   Education: {
     Education: ['Application', 'Appointment', 'Care Children (0-18)', 'Communication', 'Course / Programme', 'Early Childhood Education', 'Enrolment', 'Financial Assistance', 'Life Skills', 'Referral', 'Repairs', 'Review Hui', 'Safety', 'School', 'Support', 'Whanau Hui']
+  },
+  GDC: {},
+  Health: {
+    Addictions: [],
+    AgedCare: [],
+    Community: [],
+    Dentist: [],
+    GP: [],
+    MentalHealth: [],
+    Pharmacy: [],
+    SexualHealth: [],
+    Specialist: []
+  },
+  Housing: {
+    Housing: [],
+    Essential: [],
+    KaingaOra: [],
+    Maintenance: []
+  },
+  Iwi: {
+    Iwi: [],
+    TahaWairua: [],
+    TahaWhanau: []
+  },
+  Justice: {
+    FamilyHarm: [],
+    Corrections: [],
+    Court: [],
+    FamilyCourt: [],
+    MaoriLandCourt: [],
+    Police: [],
+    YouthJustice: []
+  },
+  MSD: {
+    WINZ: [],
+    Advance: [],
+    Benefit: [],
+    Disability: [],
+    Employment: [],
+    FoodGrant: [],
+    Housing: [],
+    Student: [],
+    TAS: [],
+    UnsupportedChild: [],
+    YouthBenefit: []
+  },
+  NGO: {
+    NGO: [],
+    TahaWairua: [],
+    TahaWhanau: []
+  },
+  OrangaTamariki: {
+    OrangaTamariki: []
+  },
+  OtherGovt: {
+    ACC: [],
+    DIA: [],
+    Immigration: [],
+    IRD: [],
+    LTNZ: []
   }
-}]
+}
